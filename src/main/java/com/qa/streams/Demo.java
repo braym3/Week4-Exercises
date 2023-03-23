699b6afee6a8f4b9d0a0b1453ace61c6fb8ca86e
Admin <Admin@192.168.1.100>
2023-03-23 16:35:05 +0000

    Added second steams task

--- src/main/java/com/qa/streams/Demo.java
+++ src/main/java/com/qa/streams/Demo.java
@@ -1,6 +1,5 @@
 package com.qa.streams;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class Demo {
@@ -9,9 +8,16 @@ public class Demo {
 
 		// Given the following List of names, using a stream, have it print "Hello " in
 		// front of each name besides "James".
-		List<String> names = Arrays.asList("Michael", "Dean", "James", "Chris");
+		List<String> names = List.of("Michael", "Dean", "James", "Chris");
 
 		names.stream().filter(name -> !name.equals("James")).forEach(System.out::println);
+
+		// Given the following List of Integers, using a stream, have it reduce and
+		// print the product (all numbers multiplied together).
+		List<Integer> nums = List.of(3, 4, 7, 8, 12);
+
+		System.out.println(nums.stream().reduce((a, b) -> a * b).get());
+
 	}
 
 }
